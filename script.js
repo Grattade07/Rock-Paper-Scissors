4f4ed7e29a560c56b9c0e219f1cb4c2ef07eeecd
Grattade07 <joe.woodcock15@hotmail.co.uk>
2022-08-13 22:00:41 +0100

    Increments added to each round result

--- script.js
+++ script.js
@@ -9,16 +9,22 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "Tie";
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore++;
         return "Lose";
     } else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
+        playerScore++;
         return "Win";
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++;
         return "Win";
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore++;
         return "Lose";
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore++;
         return "Lose";
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++;
         return "Win";
     } else if (playerSelection === "") {
         return "It appears you didn't put any option in. Please choose an option."
