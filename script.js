708c5ac885b80a39bea747f22dde9b9ce4d3c317
Grattade07 <joe.woodcock15@hotmail.co.uk>
2022-08-13 13:38:24 +0100

    playRound function works
    I have got the playRound function to work. All possible input outcomes have been added.

--- script.js
+++ script.js
@@ -4,19 +4,31 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-var playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-var computerSelection = getComputerChoice().toLowerCase();
-console.log(playerSelection);
-console.log(computerSelection);
-
 function playRound(playerSelection, computerSelection) {
-    
+    var playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+    var computerSelection = getComputerChoice().toLowerCase();
+
     if (playerSelection === computerSelection) {
-        return "It's a tie"
+        return "It's a tie";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You lose! Paper Beats Rock";
+    } else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        return "You win! Paper beats Rock";
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+        return "You lose! Scissors beats Paper";
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        return "You lose! Rock beats Scissors";
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        return "You win! Scissors beats Paper";
+    } else if (playerSelection === "") {
+        return "It appears you didn't put any option in. Please choose an option."
     }
-    else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-        return "You lose! Paper Beats Rock"
+     else {
+        return "That is not an option please choose Rock, Paper or Scissors"
     }
+
   }
    
   var theResult = playRound();
