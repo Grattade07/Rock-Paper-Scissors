cce9b3ad60a3cdc72132d08dddc5a732b40a741a
Grattade07 <joe.woodcock15@hotmail.co.uk>
2022-08-13 12:33:56 +0100

    Fixed computerChoice function
    computerChoice function fixed to return a value

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 function getComputerChoice() {
     const computerOptions = ['Rock', 'Paper', 'Scissors'];
     const computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
-    console.log (computerChoice);
+    return computerChoice;
 }
 
-const playerSelection = prompt("Rock, Paper or Scissors");
-const computerSelection = getComputerChoice();
-console.log(playerSelection)
+var playerSelection = prompt("Rock, Paper or Scissors?");
+var computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
 
 
 function playRound(playerSelection, computerSelection) {
-
-    if (playerSelection.toLowercase() === computerSelection.toLowerCase()) {
-        return ("It's a tie")
+    
+    if (playerSelection === computerSelection) {
+        return "It's a tie"
     }
     else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-        return ("You lose! Paper Beats Rock")
+        return "You lose! Paper Beats Rock"
     }
   }
    
+  var theResult = playRound(playerSelection, computerSelection)
+  console.log(theResult)
